0c613ddc8e483d4e573c14388c3b5822fe81dcbb
John Tarleton Moore <tarletonmoore@gmail.com>
2022-04-14 13:43:20 -0400

    commit tree be47fdb7603db38b7b01e7e81487bfd8a6c3061f

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const sum = target - array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === sum) return true
+
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: O(n^2)
+  space: O(n)
 */
 
 /* 
   Add your pseudocode here
+  loop through array
+  if target - a number = a number in array return true
+  if not return false
 */
 
 /*
   Add written explanation of your solution here
+  we are looping through the array and taking the target target number and subtracting one of the index numbers in a loop, if the result equals another number in the index, then the function should return true. Otherwise, it should return false.
 */
 
 // You can run `node index.js` to view these console logs
